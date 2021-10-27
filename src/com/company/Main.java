d27caf9d349aa60734148fc4e7eb554b7c324954
mrunmayee9601 <mrunmayeej99@gmail.com>
2021-10-27 13:32:35 +0530

    main file changed one line added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("This is a Devops Practical");
+        System.out.println("This a new line making changes!");
     }
 }
